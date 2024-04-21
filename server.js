065d8bb89662975ec3dd4bbee1ee9c62797c0a7d
Mahdi <mahdi02ch@gmail.com>
2024-04-21 13:19:03 +0100

    formatting

--- server.js
+++ server.js
@@ -5,26 +5,26 @@ const cors = require("cors")
 const app = express()
 app.use(cors())
 
-app.get("/photos", async (req,res) =>{
-    const albumId =  req.query.albumId
-    const response = await axios.get('https://jsonplaceholder.typicode.com/photos',{
+app.get("/photos", async (req, res) => {
+    const albumId = req.query.albumId
+    const response = await axios.get('https://jsonplaceholder.typicode.com/photos', {
         params: {
             albumId
         }
     })
     res.json(response.data)
-     
+
 })
 
-app.get("/photos/:id", async (req,res) =>{
-    const id =  req.params.id
+app.get("/photos/:id", async (req, res) => {
+    const id = req.params.id
     const response = await axios.get(`https://jsonplaceholder.typicode.com/photos/${id}`)
     res.json(response.data)
-     
+
 }
 )
 
-app.listen(3000, () => {    
+app.listen(3000, () => {
     console.log(`server on http://localhost:3000`)
 }
 )
